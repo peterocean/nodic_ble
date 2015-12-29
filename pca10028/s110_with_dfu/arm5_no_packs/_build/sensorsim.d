1cd7d93e9c151747799f0d4ddf09929508e6faff
yangyongfeng <yyf10601@163.com>
2015-12-29 17:21:53 +0800

    practice: test add an characteristic in our service

---
+++ pca10028/s110_with_dfu/arm5_no_packs/_build/sensorsim.d
@@ -0,0 +1,4 @@
+.\_build\sensorsim.o: ..\..\..\..\..\..\components\libraries\sensorsim\sensorsim.c
+.\_build\sensorsim.o: ..\..\..\..\..\..\components\libraries\sensorsim\sensorsim.h
+.\_build\sensorsim.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\_build\sensorsim.o: C:\Keil_v5\ARM\ARMCC\bin\..\include\stdbool.h
